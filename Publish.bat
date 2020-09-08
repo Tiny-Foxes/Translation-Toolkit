e9da10e9d7ca1ad56fa2610c7f7fbf601f3ca4ed
Ksempac <ksempac@gmail.com>
2020-09-08 19:24:15 +0200

    Improving the publish (no more error, and only publishing the main project, not the whole solution)

--- Publish.bat
+++ Publish.bat
@@ -2,6 +2,6 @@ if not exist ".\Publish\Linux\NUL" mkdir ".\Publish\Linux"
 
 if not exist ".\Publish\Win\NUL" mkdir ".\Publish\Win"
 
-dotnet publish TranslationToolKit.sln -r win-x64 -c Release -o ".\Publish\Win" -p:PublishSingleFile=true
+dotnet publish ".\TranslationToolKit\TranslationToolKit.csproj" -r win-x64 -c Release -o ".\Publish\Win" -p:PublishSingleFile=true
 
-dotnet publish TranslationToolKit.sln -r linux-x64 -c Release -o ".\Publish\Linux" -p:PublishSingleFile=true
+dotnet publish ".\TranslationToolKit\TranslationToolKit.csproj" -r linux-x64 -c Release -o ".\Publish\Linux" -p:PublishSingleFile=true
